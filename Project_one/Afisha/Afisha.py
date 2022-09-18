34407b264aec3e47148987367739f83692f2f286
BurdichxD4r <burdich.xd4r@gmail.com>
2022-09-18 19:13:04 +0300

    Afisha 0.3.1 (def url_page)

--- Project_one/Afisha/Afisha.py
+++ Project_one/Afisha/Afisha.py
@@ -4,25 +4,16 @@ import time
 import random
 
 
-
-
-count = 0
-performances = []
-name_data = []
-price_data = []
-date_data = []
-
-
 def url_page():
     print('''
-    Какую категорию выбирите:
-    1. Самые ожидаемые концерты
-    2. Стендап
-    3. Концерты популярной музыки
-    4. Спектакли с высоким рейтингом
-    5. Спектакли с известными актёрами
-    6. Мюзиклы
-    7. Выход из приложения
+Какую категорию выбирите:
+  1. Самые ожидаемые концерты
+  2. Стендап
+  3. Концерты популярной музыки
+  4. Спектакли с высоким рейтингом
+  5. Спектакли с известными актёрами
+  6. Мюзиклы
+  7. Выход из приложения
     ''')
     performances_dict = {
         1: 'https://afisha.yandex.ru/saint-petersburg/selections/concert-hot',
@@ -45,16 +36,22 @@ def url_page():
 
 
 def connecting_to_the_page(url_page):
+
     count = 1
+    performances = []
+
     __info = "[INFO] На данной странице, нет ни одного доступного спектакля"
     __error = '[ERROR] Некорректно введена ссылка на страницу!'
+
     while count <= 1:
         url = url_page + '?page=' + str(count)
         html_soup = BeautifulSoup(get(url).text, 'html.parser')
         check = html_soup.find_all('div', class_="page-content")
+
         if len(check) == 0:
             print(__error)
             break
+
         performances_data = html_soup.find_all('div', class_="event events-list__item yandex-sans")
         if len(performances_data) != 0:
             performances.extend(performances_data)
@@ -63,21 +60,28 @@ def connecting_to_the_page(url_page):
             print(__info)
             break
         count += 1
+    parsing(performances)
+
+def parsing(performances):
+
+    count = 0
+    name_data = []
+    price_data = []
+    date_data = []
+
+    while count <= 5:
+        info = performances[count]
+        name = info.find('h2').text
+        name_data.append(name)
+        price = info.find('span', {"data-testid": "event-card-price"}).find('span').find('span').text
+        price_data.append(price)
+        date = info.find('ul').find('li').text
+        date_data.append(date)
+        count += 1
+    demonstration(name_data, price_data, date_data)
+
+def demonstration(name_data, price_data, date_data):
+    for n in range(len(name_data)):
+        print(name_data[n], price_data[n], date_data[n])
 
 url_page()
-
-# while count <= 5:
-#     info = performances[count]
-#     name = info.find('h2').text
-#     name_data.append(name)
-#     price = info.find('span', {"data-testid": "event-card-price"}).find('span').find('span').text
-#     price_data.append(price)
-#     date = info.find('ul').find('li').text
-#     date_data.append(date)
-#     count += 1
-#
-# def demonstration():
-#     for n in range(len(name_data)):
-#         print(name_data[n], price_data[n], date_data[n])
-#
-# demonstration()
